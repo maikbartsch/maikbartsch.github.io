2aa3ec1849eff3492eb374ddc472feb0a2f220a9
maikbartsch <moclov@live.de>
2019-02-18 01:47:18 +0100

    fixed js scrolling

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -1,15 +1,40 @@
+$('a.js-scroll-trigger[href^="#"]:not([href="#"])').on('click', function(event) {
+    var target = this.hash;
+    var element = $(this.hash)
+
+    event.preventDefault();
+
+    if (element.data("transitioning")) {
+        return
+    }
+  
+    var navOffset = document.getElementById('mainNav').offsetHeight;
+    if (Math.max(0, element.offset().top - navOffset) != document.documentElement.scrollTop) {
+        element.data("transitioning", true)
+    return $('html, body').animate({
+      scrollTop: $(this.hash).offset().top - navOffset
+    }, 1e3,"easeInOutExpo", function(completion) {
+        element.data("transitioning", false)
+        /*return window.history.pushState(null, null, target);*/
+      });
+}
+  });
+
 !function(e) {
     "use strict";
-    e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    /*e('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        e.preventDefault;
         if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
             var a = e(this.hash);
             if ((a = a.length ? a : e("[name=" + this.hash.slice(1) + "]")).length)
+            if (Math.max(0, a.offset().top - 72) != document.documentElement.scrollTop) {
                 return e("html, body").animate({
                     scrollTop: a.offset().top - 72
                 }, 1e3, "easeInOutExpo"),
                 !1
+            }
         }
-    }),
+    }),*/
     e(".js-scroll-trigger").click(function() {
         e(".navbar-collapse").collapse("hide")
     }),
